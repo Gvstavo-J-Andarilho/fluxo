5c8e5926c28a93284e3bc3c9b130735768ace128
Gustavo <castromaciel17@gmail.com>
2024-08-10 19:18:39 -0300

    atualização de lançamento

--- interface projeto.py
+++ interface projeto.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 janela = Tk()
 class aplicacao():
@@ -7,12 +8,13 @@ class aplicacao():
         self .tela()
         self.frames_da_dela()
         self.botoesFrame_1()
+        self.lista_frame2()
         janela.mainloop()
 
     def tela(self): #temos todas as configurações da tela
         self.janela.title("FLUXO DE CAIXA") #titulo da janela
         self.janela.configure(background= '#1e3743') #cor de fundo
-        self.janela.geometry ("700x300") #porporção da tela
+        self.janela.geometry ("750x400") #porporção da tela
         self.janela.resizable(True, True) #flexibilidade da tela
         self.janela.maxsize(width=900,height=700)
         self.janela.minsize(width=400,height=300)
@@ -26,57 +28,91 @@ class aplicacao():
 
     def botoesFrame_1(self):
         #botão adicionar
-        self.bot_adicionar = Button(self.frame_1, text="ADICIONAR")
-        self.bot_adicionar.place(relx= 0.1, rely=0.80,relwidth=0.1 ,relheight=0.15)
+        self.bot_adicionar = Button(self.frame_1, text="ADICIONAR", bd=2, bg='#abcad9',fg="black", font=("verdana", 8, "bold"))
+        self.bot_adicionar.place(relx= 0.1, rely=0.80,relwidth=0.12 ,relheight=0.12)
 
         # botão buscar
-        self.bot_buscar = Button(self.frame_1, text="BUSCAR")
-        self.bot_buscar.place(relx=0.3, rely=0.80, relwidth=0.1, relheight=0.15)
+        self.bot_buscar = Button(self.frame_1, text="BUSCAR", bd=2, bg='#abcad9',fg="black", font=("verdana", 8, "bold"))
+        self.bot_buscar.place(relx=0.3, rely=0.80, relwidth=0.1, relheight=0.12)
 
         # botão apagar
-        self.bot_apagar = Button(self.frame_1, text="APAGAR")
-        self.bot_apagar.place(relx=0.5, rely=0.80, relwidth=0.1, relheight=0.15)
+        self.bot_apagar = Button(self.frame_1, text="APAGAR", bd=2, bg='#abcad9',fg="black", font=("verdana", 8, "bold"))
+        self.bot_apagar.place(relx=0.5, rely=0.80, relwidth=0.1, relheight=0.12)
 
         # botão editar
-        self.bot_editar = Button(self.frame_1, text="EDITAR")
-        self.bot_editar.place(relx=0.7, rely=0.80, relwidth=0.1, relheight=0.15)
+        self.bot_editar = Button(self.frame_1, text="EDITAR", bd=2, bg='#abcad9',fg="black", font=("verdana", 8, "bold"))
+        self.bot_editar.place(relx=0.7, rely=0.80, relwidth=0.1, relheight=0.12)
 
+        # botão entrada
+        self.bot_entrada = Button(self.frame_1, text="INSERIR ENTRADA", bd=2, bg='#48ab4d', fg="black",
+                                 font=("verdana", 8, "bold"))
+        self.bot_entrada.place(relx=0.5, rely=0.65, relwidth=0.20, relheight=0.10)
+
+        # botão saida
+        self.bot_saida = Button(self.frame_1, text="INSERIR SAIDA", bd=2, bg='#ab4848', fg="black",
+                                 font=("verdana", 8, "bold"))
+        self.bot_saida.place(relx=0.25, rely=0.65, relwidth=0.20, relheight=0.10)
 
     #criando as label e entradas de dados:
 
         # label busca
-        self.lb_buscar = Label(self.frame_1, text="procurar",bg='#436778')
-        self.lb_buscar.place(relx=0.025, rely=0.01)
+        self.lb_buscar = Label(self.frame_1, text="Procurar",bg='#436778', fg="black", font=("verdana", 9, "bold"))
+        self.lb_buscar.place(relx=0.015, rely=0.01)
         #entrada busca
         self.buscar_entry= Entry(self.frame_1)
         self.buscar_entry.place(relx=0.01,rely=0.15,relwidth=0.1, relheight=0.15)
 
-# label nome
-        self.lb_nome = Label(self.frame_1, text="Nome",bg='#436778')
-        self.lb_nome.place(relx=0.15, rely=0.01)
+        # label nome
+        self.lb_nome = Label(self.frame_1, text="Nome",bg='#436778', fg="black", font=("verdana", 11, "bold"))
+        self.lb_nome.place(relx=0.15, rely=0.15)
         #entrada nome
         self.nome_entry= Entry(self.frame_1)
-        self.nome_entry.place(relx=0.15,rely=0.15,relwidth=0.25, relheight=0.15)
+        self.nome_entry.place(relx=0.15,rely=0.30,relwidth=0.25, relheight=0.10)
+
+        # label valor
+        self.lb_valor = Label(self.frame_1, text="Valor", bg='#436778', fg="black", font=("verdana", 9, "bold"))
+        self.lb_valor.place(relx=0.45, rely=0.42)
+        # entrada busca
+        self.valor_entry = Entry(self.frame_1)
+        self.valor_entry.place(relx=0.40, rely=0.53, relwidth=0.15, relheight=0.10)
 
         # label dia
-        self.lb_dia = Label(self.frame_1, text="dia", bg='#436778')
-        self.lb_dia.place(relx=0.45, rely=0.01)
+        self.lb_dia = Label(self.frame_1, text="Dia", bg='#436778')
+        self.lb_dia.place(relx=0.45, rely=0.15)
         # entrada dia
         self.dia_entry = Entry(self.frame_1)
-        self.dia_entry.place(relx=0.45, rely=0.15, relwidth=0.05, relheight=0.15)
+        self.dia_entry.place(relx=0.45, rely=0.30, relwidth=0.05, relheight=0.10)
 
         # label mês
-        self.lb_mes = Label(self.frame_1, text="mês", bg='#436778')
-        self.lb_mes.place(relx=0.55, rely=0.01)
+        self.lb_mes = Label(self.frame_1, text="Mês", bg='#436778')
+        self.lb_mes.place(relx=0.55, rely=0.15)
         # entrada mês
         self.mes_entry = Entry(self.frame_1)
-        self.mes_entry.place(relx=0.55, rely=0.15, relwidth=0.05, relheight=0.15)
- # label ano
-        self.lb_ano = Label(self.frame_1, text="ano", bg='#436778')
-        self.lb_ano.place(relx=0.65, rely=0.01)
+        self.mes_entry.place(relx=0.55, rely=0.30, relwidth=0.05, relheight=0.10)
+
+        # label ano
+        self.lb_ano = Label(self.frame_1, text="Ano", bg='#436778')
+        self.lb_ano.place(relx=0.65, rely=0.15)
         # entrada ano
         self.ano_entry = Entry(self.frame_1)
-        self.ano_entry.place(relx=0.65, rely=0.15, relwidth=0.10, relheight=0.15)
+        self.ano_entry.place(relx=0.65, rely=0.30, relwidth=0.10, relheight=0.10)
 
+
+#criação das listas de lançamento
+    def lista_frame2(self):
+        self.lanca_frame2 = ttk.Treeview(self.frame_2, height=3, column=("col1", "col2", "col3", "col4"))
+        self.lanca_frame2.heading("#0", text="Data")
+        self.lanca_frame2.heading("#1", text="Valor")
+        self.lanca_frame2.heading("#2", text="Tipo")
+        self.lanca_frame2.heading("#3", text="Nome")
+        self.lanca_frame2.heading("#4", text="Saldo")
+
+        self.lanca_frame2.column("#0", width = 150)
+        self.lanca_frame2.column("#1", width=100)
+        self.lanca_frame2.column("#2", width=50)
+        self.lanca_frame2.column("#3", width=150)
+        self.lanca_frame2.column("#4", width=30)
+
+        self.lanca_frame2.place(relx=0.01, rely=0.1, relwidth=0.95,relheight=0.85)
 
 aplicacao()
